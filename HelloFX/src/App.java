f82c73fcd806c02aa4e9ad377daf55407263af54
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2023-05-09 14:19:28 +0530

    new jx

--- HelloFX/src/App.java
+++ HelloFX/src/App.java
@@ -1,35 +1,22 @@
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
- 
-public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent arg0) {
-                System.out.println("Hello World!");
-            }
-            
-        });
+public class App extends Application{
+
+    public static void main(String[] args) {
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        Group root = new Group();
+        Scene scene = new Scene(root,800,500);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
- 
- public static void main(String[] args) {
-        launch(args);
     }
 }
